ed527c322c9995f8ec19cb5a310c07232a3c6860
islamariful <60264222+islamariful@users.noreply.github.com>
2020-05-10 18:24:01 -0400

    Add files via upload

---
+++ pokedex.swift
@@ -0,0 +1,12 @@
+//
+//  pokedex.swift
+//  Pokedex
+//
+//  Created by Ariful Islam on 2020-05-02.
+//  Copyright © 2020 CS50. All rights reserved.
+//
+
+import Foundation
+struct Pokedex {
+    var caught = [String:Bool]()
+}
